59242dc04d04a2df4a1a652207865abf7b0222f3
leechanggi <67743720+leechanggi@users.noreply.github.com>
2022-06-08 18:32:30 +0900

    final

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,38 +1,49 @@
 import React from 'react';
-import { useSetRecoilState } from 'recoil';
+import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
 import { IToDo, toDoState, Categories } from './atoms';
 
 function ToDo({ text, id, category }: IToDo) {
-  const setToDos = useSetRecoilState(toDoState);
-  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+  const [toDos, setToDos] = useRecoilState(toDoState);
+
+  const addList = (event: React.MouseEvent<HTMLButtonElement>) => {
     const {
       currentTarget: { name },
     } = event;
     setToDos(oldToDos => {
       const targetIndex = oldToDos.findIndex(toDo => toDo.id === id);
-      const oldToDo = oldToDos[targetIndex];
       const newToDo = { text, id, category: name as any };
       return [...oldToDos.slice(0, targetIndex), newToDo, ...oldToDos.slice(targetIndex + 1)];
     });
   };
+
+  const delList = () => {
+    setToDos(oldToDos => {
+      const targetIndex = oldToDos.findIndex(toDo => toDo.id === id);
+      return [...oldToDos.slice(0, targetIndex), ...oldToDos.slice(targetIndex + 1)];
+    });
+  };
+
   return (
     <li>
       {text}
       {category !== Categories.DOING && (
-        <button type="button" name={Categories.DOING} onClick={onClick}>
+        <button type="button" name={Categories.DOING} onClick={addList}>
           DOING
         </button>
       )}
       {category !== Categories.TO_DO && (
-        <button type="button" name={Categories.TO_DO} onClick={onClick}>
+        <button type="button" name={Categories.TO_DO} onClick={addList}>
           To Do
         </button>
       )}
       {category !== Categories.DONE && (
-        <button type="button" name={Categories.DONE} onClick={onClick}>
+        <button type="button" name={Categories.DONE} onClick={addList}>
           DONE
         </button>
       )}
+      <button type="button" onClick={delList}>
+        DEL
+      </button>
     </li>
   );
 }
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -6,12 +6,32 @@ import CreateToDo from './CreateToDo';
 import ToDo from './ToDo';
 
 function ToDoList() {
-  const toDos = useRecoilValue(toDoSelector);
+  const [toDos, setToDos] = useRecoilState(toDoState);
+  const toDosSel = useRecoilValue(toDoSelector);
 
   const [category, setCategory] = useRecoilState(categoryState);
 
   const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
     setCategory(event.currentTarget.value as any);
+  };
+
+  const saveData = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const toDosString = JSON.stringify(toDos);
+    if (toDos.length) {
+      window.localStorage.setItem('toDos', toDosString);
+    }
+  };
+
+  const callData = (event: React.MouseEvent<HTMLButtonElement>) => {
+    if (window.localStorage.getItem('toDos') !== null) {
+      const toDosString = window.localStorage.getItem('toDos');
+      const toDosCalled = JSON.parse(toDosString as any);
+      setToDos(toDosCalled);
+    }
+  };
+
+  const removeData = (event: React.MouseEvent<HTMLButtonElement>) => {
+    localStorage.removeItem('toDos');
   };
 
   return (
@@ -23,9 +43,18 @@ function ToDoList() {
         <option value={Categories.DONE}>Done</option>
       </select>
       <CreateToDo />
-      {toDos?.map(toDo => (
+      {toDosSel?.map(toDo => (
         <ToDo key={toDo.id} {...toDo} />
       ))}
+      <button type="button" onClick={saveData}>
+        Save
+      </button>
+      <button type="button" onClick={callData}>
+        Load
+      </button>
+      <button type="button" onClick={removeData}>
+        Reset
+      </button>
     </div>
   );
 }
